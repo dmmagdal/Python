5bf5590033128870e8b91dc0e11f08cd0d5ef69b
Diego Magdaleno <dmmagdal@ucsc.edu>
2017-12-03 00:33:58 -0800

    more tutorial stuff with nltk

---
+++ NLProcessing/tokenizeExample7.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+# experiment with chinking, which is very similar to chunking, as a basic way to remove a chunk from a chunk. The chunk data removed is called a chink
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")                                                                    # the following are training and testing data which are the 2005 and 2006 (respectively) state of the union speeches by President George W Bush 
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try: 
+        for i in tokenized:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            chunkGram = r"""Chunk: {<.*>+}
+                                        }<VB.?|IN|DT|TO>+{"""   
+                                                                                                                    # now, we're removing from the chink one or more verbs, prepositions, determiners, or the word "to"
+            chunkParser = nltk.RegexpParser(chunkGram)
+            chunked = chunkParser.parse(tagged)
+            chunked.draw()
+    except Exception  as e:
+        print(str(e))
+
+process_content()
