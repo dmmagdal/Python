e31153d9778c7bf07b7b0158b3cd962ebbef9e30
Diego Magdaleno <dmmagdal@ucsc.edu>
2018-08-05 18:47:08 -0700

    update nlp processing

--- NLProcessing/tokenizeExample.py
+++
@@ -1,5 +0,0 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-Example_Text = "Hello, Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."      # example text to be tokenized
-
-print(sent_tokenize(Example_Text))                                                                                                                                  # print a tokenization of the text (by sentence)
