64847df978cf6fd288c32bb77780703f739b7e3f
Diego Magdaleno <dmmagdal@ucsc.edu>
2018-11-08 23:05:39 -0800

    began work on packetsniffing program in pentest. 99 % finished with network setup program for wrench2

--- wrench2/netsetup.py
+++ wrench2/netsetup.py
@@ -11,20 +11,13 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.chrome.options import Options
 
 def main():
-	# Create webdriver object (run in headless).
-	# firefox_options = Options()
-	# firefox_options.add_argument('--headless')
-	# firefox_options.add_argument('--window-size=1920x1080')
-	# firefox_driver = webdriver.Firefox(firefox_options=firefox_options)
-
 	# Initialize Chrome selenium web driver.
 	chromeOpts = Options()
 	#opts.binary_location = chromium_path
-	# chromeOpts.add_argument('--headless')
+	chromeOpts.add_argument('--headless')
 	chromeOpts.add_argument('--incognito')
 	chromeOpts.add_argument('--window-size=1920x1080')
 	#chrome_driver = webdriver.Chrome(options=chromeOpts, chrome_options=opts)
@@ -34,29 +27,10 @@ def main():
 	# Interact with webpage.
 	url = "https://resreg.ucsc.edu:9443/clientStatus.!%5E"
 	url2 = "https://www.ucsc.edu/"
-	# firefox_driver.get(url)
 	chrome_driver.get(url)
 
 	# If the page is not redirected to the ucsc site.
-	# if firefox_driver.currenturl() != url2:
-	# 	# Open file with login crentials.
-	# 	login = open("credentials.txt", 'r')
-	# 	lines = login.readlines()
-	# 	username = lines[0].split("\r\n")[0]
-	# 	password = lines[1].split("\r\n")[0]
-	# 	login.close()
-	# 	# Use login credentials on webpage.
-	# 	idbox = firefox_driver.find_element_by_id("")
-	# 	idbox.click()
-	# 	idbox.send_keys(username)
-	# 	pwdbox = firefox_driver.find_element_by_id("")
-	# 	pwdbox.click()
-	# 	pwdbox.send_keys(password)
-	# 	checkbutton = firefox_driver.find_element_by_id("")
-	# 	checkbutton.click()
-	# 	enterbutton = firefox_driver.find_element_by_id("")
-	# 	enterbutton.click()
-	if chrome_driver.currenturl() != url2:
+	if chrome_driver.current_url != url2:
 		# Open file with login crentials.
 		login = open("credentials.txt", 'r')
 		lines = login.readlines()
@@ -64,25 +38,26 @@ def main():
 		password = lines[1].split("\r\n")[0]
 		login.close()
 		# Use login credentials on webpage.
-		idbox = chrome_driver.find_element_by_id("")
+		idbox = chrome_driver.find_element_by_name("userId")
 		idbox.click()
 		idbox.send_keys(username)
-		pwdbox = chrome_driver.find_element_by_id("")
+		pwdbox = chrome_driver.find_element_by_name("pass")
 		pwdbox.click()
 		pwdbox.send_keys(password)
-		checkbutton = chrome_driver.find_element_by_id("")
+		checkbutton = chrome_driver.find_element_by_name("accptPlcy")
 		checkbutton.click()
-		enterbutton = chrome_driver.find_element_by_id("")
+		enterbutton = chrome_driver.find_element_by_id("subBtn")
 		enterbutton.click()
 
 	# Close the webdriver, the wifi is setup.
-	# firefox_driver.close()
 	chrome_driver.close()
 
 	## Send out the ip address.
 	#os.system("ifconfig > network.txt")
 
+	# Print status of program and exit.
 	print("Wifi configured.")
+	exit(0)
 
 
 if __name__ == '__main__':
