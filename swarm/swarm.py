890d2749ebece004286adea87dbce8a24eb3cc75
Diego Magdaleno <dmmagdal@ucsc.edu>
2018-12-15 22:15:02 -0800

    finished ability to create ships in starting formation.

--- swarm/swarm.py
+++ swarm/swarm.py
@@ -2,7 +2,11 @@
 # author: Diego Magdaleno
 # create a game instance of objects and a swarm to navigate between
 # obstacles.
-# game:
+# game: obstacles are randomly generated and tested for the existance
+# 	of a solution (can a ship navigate to the other side). Ships are
+#	then generated in formation and made to fly across the obstacle
+#	course in a group. The exercise here is to see how effective a
+#	swarm is in navigating obstacles vs ships navigating individually.
 # Python 3.6
 # Windows 10
 
@@ -35,22 +39,21 @@ class Obstacle:
 		else:
 			self.center = center
 		# Set cornerpoints.
-		if cornerPoints == None and shape == "traingle":
+		if cornerPoints == None and self.shape == "traingle":
 			corners = self.generateCorners(3, self.center)
 			self.cornerPoints = corners 
-		elif cornerPoints == None and shape == "rectangle":
+		elif cornerPoints == None and self.shape == "rectangle":
 			corners = self.generateCorners(4, self.center)
 			self.cornerPoints = corners
-		elif cornerPoints == None and shape == "polygon":
+		elif cornerPoints == None and self.shape == "polygon":
 			numSides = random.randint(4,25)
 			corners = self.generateCorners(numSides, self.center)
 			self.cornerPoints = corners
-		elif cornerPoints == None and shape == "ellipse":
+		elif cornerPoints == None and self.shape == "ellipse":
 			circumfPoints = self.genEllipseCircPts(self.center)
 			self.cornerPoints = circumfPoints
 		else:
 			self.cornerPoints = cornerPoints
-		print(self.cornerPoints)
 
 
 	# For randomly generated obstacles, this will used the number of
@@ -82,20 +85,20 @@ class Obstacle:
 		else:
 			for i in range(numSides):
 				if i < numSides//4:
-					xcorner = xcoord + random.randint(25)
-					ycorner = ycoord + random.randint(25)
+					xcorner = xcoord + random.randint(5, 25)
+					ycorner = ycoord + random.randint(5, 25)
 					cornersList.append([xcorner, ycorner])
 				elif i > numSides//4 and i < numSides//2:
-					xcorner = xcoord - random.randint(25)
-					ycorner = ycoord + random.randint(25)
+					xcorner = xcoord - random.randint(5, 25)
+					ycorner = ycoord + random.randint(5, 25)
 					cornersList.append([xcorner, ycorner])
 				elif i > numSides//2 and i < numSides * 3//4:
-					xcorner = xcoord - random.randint(25)
-					ycorner = ycoord - random.randint(25)
+					xcorner = xcoord - random.randint(5, 25)
+					ycorner = ycoord - random.randint(5, 25)
 					cornersList.append([xcorner, ycorner])
 				else:
-					xcorner = xcoord + random.randint(25)
-					ycorner = ycoord - random.randint(25)
+					xcorner = xcoord + random.randint(5, 25)
+					ycorner = ycoord - random.randint(5, 25)
 					cornersList.append([xcorner, ycorner])
 		# Return corners list.
 		return cornersList
@@ -120,7 +123,8 @@ class Obstacle:
 			for i in range(int(n)):
 				xpoint = xcoord + int(math.cos(2*math.pi/n*i)*a)
 				ypoint = ycoord + int(math.sin(2*math.pi/n*i)*a)
-				cornersList.append([xpoint,ypoint])
+				if [xpoint,ypoint] not in cornersList:
+					cornersList.append([xpoint,ypoint])
 		# Otherwise, treat the obstacle as an ellipse.
 		else:
 			numerator = math.pow(a-b, 2)
@@ -132,7 +136,8 @@ class Obstacle:
 			for i in range(int(n)):
 				xpoint = xcoord + int(math.cos(2*math.pi/n*i)*a)
 				ypoint = ycoord + int(math.sin(2*math.pi/n*i)*b)
-				cornersList.append([xpoint,ypoint])
+				if [xpoint,ypoint] not in cornersList:
+					cornersList.append([xpoint,ypoint])
 		# Return corners list.
 		return cornersList
 
@@ -141,6 +146,10 @@ class Obstacle:
 	def draw(self):
 		# Create turtle object to draw the obstacle.
 		objTur = turtle.Turtle()
+		objTur.setx(self.center[0])
+		objTur.sety(self.center[1])
+		objTur.shape("circle")
+		objTur.shapesize(0.01,0.01)
 		objTur.color("white")
 		objTur.pensize(1)
 		objTur.penup()
@@ -165,21 +174,24 @@ class Ship:
 	ycoord = 0
 	shipNum = 0
 
-	def __init__(self, center=None, shipN=None, size=None, numS=None):
-		if center == None and size != None:
-			self.xcoord = random.randint(5, size-5)
-			self.ycoord = random.randint(5, size-5)
-		elif center != None:
+	def __init__(self, center=None, shipN=None, numS=None):
+		if center == None:
+			self.xcoord = random.randint(5, 25)
+			self.ycoord = random.randint(5, 745)
+		else:
 			self.xcoord = center[0]
 			self.ycoord = center[1]
-		if shipNum == None:
+		if shipN == None:
 			self.shipNum = random.randint(1, numS)
 		else:
 			self.shipNum = shipN
 
 
-	def draw():
+	# Draw ship with turtle.
+	def draw(self):
 		ship = turtle.Turtle()
+		ship.setx(self.xcoord)
+		ship.sety(self.ycoord)
 		ship.color("red")
 		ship.penup()
 
@@ -200,7 +212,7 @@ def main():
 	# Argument variables.
 	debug = False
 	numDrones = 1
-	numObj = random.randint(1, 25)
+	numObj = random.randint(75, 200)
 
 	# If there are 2 command line arguments.
 	if len(sys.argv) == 2:
@@ -236,18 +248,27 @@ def main():
 		print("There are "+str(numObj)+" number of obstacles.")
 
 		# Check if intitializing obstacles works.
-		obj0 = [Obstacle("traingle")]
+		#obj0 = [Obstacle("traingle")]
 		#print(obj0[0].center)
 		#print(obj0[0].shape)
 		#print(obj0[0].cornerPoints)
 
+		# Check if initializing ships works.
+		#ship0 = [Ship(center=None, shipN=None, numS=numDrones)]
+		#print(ship1[0].xcoord)
+		#print(ship1[0].ycoord)
+		#print(ship1[0].shipNum)
+
 		# Check if the generateObjects() method works (ie, obstacles
 		# can be initialized without any issues.)
+		ship1 = createFleet(numDrones)
 		obj1 = generateObjects(numObj)
 		# Check if the drawScreen() method works (ie, can objects be
 		# drwan to the canvas without any issues.)
 		#drawScreen(obj0)
-		drawScreen(obj1)
+		#drawScreen(obj1)
+		#drawScreen([], ship0)
+		drawScreen(obj1, ship1)
 
 	'''
 	# Create a list of obstacles and store that.
@@ -257,6 +278,77 @@ def main():
 	while not solutionExists(listOfObj):
 		listOfObj = generateObjects()
 	'''
+
+
+# Initialize a fleet of objects in delta formations with each squadron
+# consisting of 6 ships.
+def createFleet(numDrones):
+	# Initialize an empty list of ship objects.
+	shipList = []
+	# Initialize a leader variable that temporarily marks which ship is
+	# the leader of a squadron.
+	leader = None
+	# Initialize a counter for the number of squadrons.
+	squadNum = 0
+	# Initialize a list that holds the left and right y values that
+	# represent the edges of of a squadron's formation. Values of the
+	# edge list are stored in lists [leftedge, rightedge]
+	edgeList = []
+	for i in range(numDrones):
+		mod = i % 6
+		if mod == 0:
+			# Randomly initialize the starting x,y coordinates of the
+			# squadron leader. 
+			xstart = random.randint(35, 50)
+			ystart = random.randint(25, 725)
+			# As long as the ystart value is within an edgelist of
+			# another squadron, continue to randomize it.
+			while inEdgeList(edgeList, ystart):
+				ystart = random.randint(25, 725)
+			# Increment the squadron number.
+			squadNum = squadNum+1
+			# Initialize the Ship object for the squadron leader. Then
+			# append it to the list of ships.
+			leader = Ship([xstart, ystart], i)
+			shipList.append(leader)
+		elif mod == 1:
+			xstart = leader.xcoord-12
+			ystart = leader.ycoord+7
+			shipList.append(Ship([xstart, ystart], i))
+		elif mod == 2:
+			xstart = leader.xcoord-12
+			ystart = leader.ycoord-7
+			shipList.append(Ship([xstart, ystart], i))
+		elif mod == 3:
+			xstart = leader.xcoord-24
+			ystart = leader.ycoord
+			shipList.append(Ship([xstart, ystart], i))
+		elif mod == 4:
+			xstart = leader.xcoord-24
+			ystart = leader.ycoord+14
+			shipList.append(Ship([xstart, ystart], i))
+		elif mod == 5:
+			xstart = leader.xcoord-24
+			ystart = leader.ycoord-14
+			shipList.append(Ship([xstart, ystart], i))
+	# Return the objects list.
+	return shipList
+
+
+# Checks to see if the yvalue of the squadron's leader is within
+# the y values of an existing sqaudron and is within a safe distance
+# away from other squadrons.
+def inEdgeList(edgeList, ystart):
+	# Iterate through the edge list.
+	for i in edgeList:
+		# If the ystart value is less than the left edge - 35 (35 being
+		# the safety distance between squadrons) or is greater than the
+		# right edge + 35, then return False.
+		if ystart >= i[0]-35 or ystart <= i[1]+35:
+			return True
+	# Otherwise, the ystart value is not within any squadron. Return
+	# True.
+	return False
 
 
 # Generate a list of obstacles in the map.
@@ -276,7 +368,7 @@ def solutionExists(objList):
 
 
 # Draw the current screen.
-def drawScreen(obstacleList,):
+def drawScreen(obstacleList, shipList):
 	win = turtle.Screen()
 	win.title("Swarm Game")
 	win.setup(width=750, height=750, startx=25, starty=25)
@@ -287,9 +379,13 @@ def drawScreen(obstacleList,):
 	# Draw obstacles.
 	for ob in obstacleList:
 		ob.draw()
-		print(ob.center)
-		print(ob.shape)
-		print(ob.cornerPoints)
+		#print(ob.center)
+		#print(ob.shape)
+		#print(ob.cornerPoints)
+
+	# Draw ships.
+	for sh in shipList:
+		sh.draw()
 
 	#win.exitonclick()
 	win.mainloop()
