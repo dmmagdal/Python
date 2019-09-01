a41a695dafe7a4f6dd52676a27c946c5e2edb1e6
Diego Magdaleno <dmmagdal@ucsc.edu>
2019-09-01 09:58:19 -0700

    managed to get the player ships to move.

--- turtleInvaders/turtleInvaders.py
+++ turtleInvaders/turtleInvaders.py
@@ -13,10 +13,10 @@ import math
 import time
 
 class Ship:
-	xcoord = 0
-	ycoord = 0
-	shipTurtle = None
-	hitbox = [] 
+	# xcoord = 0
+	# ycoord = 0
+	# shipTurtle = None
+	# hitbox = [] 
 
 	def __init__(self, center, color, shipType=None):
 		self.xcoord = center[0]
@@ -26,6 +26,7 @@ class Ship:
 		self.shipTurtle.sety(self.ycoord)
 		self.shipTurtle.color(color)
 		self.shipTurtle.penup()
+		self.shipTurtle.showturtle()
 		if shipType == None:
 			self.shipTurtle.speed(1)
 			#self.shipTurtle.rotate(90)
@@ -37,6 +38,17 @@ class Ship:
 		self.updateHitBox(self.xcoord, self.ycoord)
 
 
+	def move(self, xmove, ymove):
+		self.xcoord += xmove
+		self.ycoord += ymove
+		if self.xcoord > 725 or self.xcoord < 25:
+			self.xcoord -= xmove
+		if self.ycoord < 200:
+			self.ycoord -= ymove
+		self.shipTurtle.goto(self.xcoord, self.ycoord)
+		self.updateHitBox(self.xcoord, self.ycoord)
+
+
 	def updateHitBox(self, xcenter, ycenter):
 		corner1 = (xcenter+3, ycenter+6)
 		corner2 = (xcenter-3, ycenter+6)
@@ -45,26 +57,15 @@ class Ship:
 		self.hitbox = [corner1, corner2, corner3, corner4]
 
 
-	def move(self, xmove, ymove):
-		xcoord += xmove
-		ycoord += ymove
-		if self.xcoord > 725 or self.xcoord < 25:
-			self.xcoord -= xmove
-		if self.ycoord < 200:
-			self.ycoord -= ymove
-		self.shipTurtle.goto(xcoord, ycoord)
-		updateHitBox(xcoord, ycoord)
-
-
 	def fire(self, color):
 		return Blaster([xcoord, ycoord], color)
 
 
 
 class Blaster:
-	xcoord = 0
-	ycoord = 0
-	blastTurtle = None
+	# xcoord = 0
+	# ycoord = 0
+	# blastTurtle = None
 
 	def __init__(self, center, color):
 		self.xcoord = center[0]
@@ -75,11 +76,12 @@ class Blaster:
 		self.blastTurtle.color(color)
 		self.blastTurtle.penup()
 		self.blastTurtle.shape("square")
+		self.blastTurtle.showturtle()
 
 
 	def move(self):
-		ycoord += 3
-		self.blastTurtle.goto(xcoord, ycoord)
+		self.ycoord += 3
+		self.blastTurtle.goto(self.xcoord, self.ycoord)
 
 
 
@@ -95,7 +97,7 @@ def main():
 		exit(1)
 	else:
 		# Check that last sys arg is correct.
-		if int(sys.argv[1]) > 2 or int(sys.argv[1]) < 1:
+		if int(sys.argv[1]) != 2 and int(sys.argv[1]) != 1:
 			str1 = "Error: Please enter either "
 			str2 = "1 or 2 for the number of players"
 			print(str1 + str2)
@@ -105,10 +107,7 @@ def main():
 
 	# Set player variables based on sys args.
 	if numPlayers == 2:
-		score.append(9)
-
-	for sh in range(len(score)):
-		ships.append(Ship([350, 700], "white"))
+		score.append(0)
 
 	# Create screen object.
 	win = turtle.Screen()
@@ -117,16 +116,28 @@ def main():
 	win.bgcolor("black")
 	win.setworldcoordinates(0, 0, 750, 750)
 
+	for sh in range(len(score)):
+		color = "white"
+		if sh // 1 == 1:
+			color = "green"
+		if sh == 0 and len(score) == 2:
+			ships.append(Ship([300, 50], color))
+		elif sh == 1 and len(score) == 2:
+			ships.append(Ship([400, 50], color))
+		else:
+			# sh == 0 and len(score) == 1.
+			ships.append(Ship([350, 50], color))
+
 	if numPlayers == 2:
-		win.onkey(lambda: ships[0].move(-1, 0), "Left")
-		win.onkey(lambda: ships[0].move(1, 0), "Right")
-		win.onkey(lambda: ships[1].move(-1, 0), "a")
-		win.onkey(lambda: ships[1].move(1, 0), "d")
+		win.onkeypress(lambda: ships[0].move(-1, 0), "Left")
+		win.onkeypress(lambda: ships[0].move(1, 0), "Right")
+		win.onkeypress(lambda: ships[1].move(-1, 0), "a")
+		win.onkeypress(lambda: ships[1].move(1, 0), "d")
 	else:
-		win.onkey(lambda: ships[1].move(-1, 0), "a")
-		win.onkey(lambda: ships[1].move(1, 0), "d")
+		win.onkeypress(lambda: ships[0].move(-5, 0), "a")
+		win.onkeypress(lambda: ships[0].move(5, 0), "d")
 
-
+	win.listen()
 	win.mainloop()
 
 
